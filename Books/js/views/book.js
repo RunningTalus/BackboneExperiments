b04dcf8b748adb2e412f62d6b330b584e0529bb6
Dave May <dave@davemay.org>
2015-10-22 13:37:45 -0600

    Removing Models from the BookView.

--- Books/js/views/book.js
+++ Books/js/views/book.js
@@ -8,11 +8,23 @@ app.BookView = Backbone.View.extend({
    className: 'bookContainer',
    template: _.template( $('#bookTemplate').html() ),
 
+    events: {
+        'click .delete': 'deleteBook'
+    },
+
    render: function() {
        // this.el is what we defined in tagName, use $el to get access to jQuery html() function
        this.$el.html( this.template( this.model.toJSON() ));
 
        return this;
    },
+
+    deleteBook: function() {
+        // Delete model
+        this.model.destroy();
+
+        // Delete view
+        this.remove();
+    }
 
 });
