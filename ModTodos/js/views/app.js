05b8b01650048f69dc5fa01a81853d997a85718a
Dave May <dave@davemay.org>
2015-10-25 12:08:23 -0600

    Completed the AppView/main view for the project.

---
+++ ModTodos/js/views/app.js
@@ -0,0 +1,100 @@
+// Backbone/ModTodos/js/views/app.js
+// AppView
+
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'collections/todos',
+    'views/todo',
+    'text!templates/stats.html'
+], function($, _, Backbone, Todos, TodoView, statsTemplate ){
+
+    var AppView = Backbone.View.extend({
+
+        // Instead of generating a new element, bind to the existing skeleton of the app already present in the HTML.
+        el: $('#todoapp'),
+
+        // Our template for the line of statistics at the bootom of the app.
+        statsTemplate: _.template(statsTemplate),
+
+        // Delegated events for creating new items, and clearing completed ones.
+        events: {
+            "keypress #new-todo":  "createOnEnter",
+            "keyup #new-todo":     "showTooltip",
+            "click .todo-clear a": "clearCompleted"
+        },
+
+        // At initialization we listen to the relevant events on the `Todos` collection, when items are added or changed.
+        // This collection is passed on the constructor of this AppView.
+        // Kick things off by loading any preexisting todos that might be saved in *localStorage*.
+        initialize: function() {
+            this.input    = this.$("#new-todo");
+
+            this.listenTo(this.collection, 'add', this.addOne);
+            this.listenTo(this.collection, 'reset', this.addAll);
+            this.listenTo(this.collection, 'all', this.render);
+
+            this.collection.fetch();
+        },
+
+        // Re-rendering the App just means refreshing the statistics -- the rest of the app doesn't change.
+        render: function() {
+            var done = this.collection.done().length;
+            this.$('#todo-stats').html(this.statsTemplate({
+                total:      this.collection.length,
+                done:       this.collection.done().length,
+                remaining:  this.collection.remaining().length
+            }));
+        },
+
+        // Add a single todo item to the list by creating a view for it, and appending its element to the `<ul>`.
+        addOne: function(todo) {
+            var view = new TodoView({model: todo});
+            this.$('#todo-list').append(view.render().el);
+        },
+
+        // Add all items in the **Todos** collection at once.
+        addAll: function() {
+            this.collection.each(this.addOne);
+        },
+
+        // Generate the attributes for a new Todo item.
+        newAttributes: function() {
+            return {
+                content: this.input.val(),
+                order:   this.collection.nextOrder(),
+                done:    false
+            };
+        },
+
+        // If you hit return in the main input field, create new **Todo** model, persisting it to *localStorage*.
+        createOnEnter: function(e) {
+            if (e.keyCode != 13) return;
+            this.collection.create(this.newAttributes());
+            this.input.val('');
+        },
+
+        // Clear all done todo items, destroying their models.
+        clearCompleted: function() {
+            _.each(this.collection.done(), function(todo){ todo.clear(); });
+            return false;
+        },
+
+        // Lazily show the tooltip that tells you to press `enter` to save a new todo item, after one second.
+        showTooltip: function() {
+            var tooltip = this.$('.ui-tooltip-top'),
+                val = this.input.val(),
+                show;
+            tooltip.fadeOut();
+            if (this.tooltipTimeout) clearTimeout(this.tooltipTimeout);
+            if (val === '' || val === this.input.attr('placeholder')) return;
+            show = function(){ tooltip.show().fadeIn(); };
+            this.tooltipTimeout = _.delay(show, 1000);
+        }
+
+    });
+
+    return AppView;
+
+});
